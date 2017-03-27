e90d5e7209cd716256f6a9b2936fb6e37b9a0dac
Daan Theunis <Daan.Theunis@student.pxl.be>
2017-03-27 15:53:08 +0200

    Gallery

--- StefRoets/public_html/assets/js/gallery.js
+++ StefRoets/public_html/assets/js/gallery.js
@@ -1,5 +1,8 @@
 function openGallery(clicky)
 {
+    if (clicky != "buitenschrijnwerk" && clicky != "interieurs" && clicky != "events"){
     window.location.href = "galleries/"+clicky+".html";
+} else {
     window.location.href = clicky+".html";
-} 
+}
+}
