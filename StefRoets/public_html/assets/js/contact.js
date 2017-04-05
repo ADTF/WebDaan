3af0312cf2c25a7f4c2ba836a1a6e6d3615a94ca
Tweeksay <tweekziee@outlook.com>
2017-04-05 14:27:17 +0200

    Codesnip

--- StefRoets/public_html/assets/js/contact.js
+++ StefRoets/public_html/assets/js/contact.js
@@ -15,106 +15,38 @@ $(document).ready(function() {
 		};
 	}
 		
-	initializeButtons();
 	
+        
 	initTextarea();
-		
-	$("#form-section-next").click(function() {
-		
-		enterPressed();
-	});
-	
-	$("#form-section-submit").click(function() {
-		console.log("Sending form");
-		
-		$("#form-input-submit").click();
-	});
-	
-	function enterPressed() {
-		var i = 0;
-		while ( i < $("#form-step-" + _counter).attr("data-input-number")) {
-			console.log("Result input check: " + checkInput(_counter, i));
-			if (!checkInput(_counter, i)) {
-				focusNextInput();
-				console.log("Going to next input");
-				return false;
-			}
-			i++;
+        function initTextarea() {
+            var text = document.getElementById('input-5-0');
+		function resize () {
+                    text.style.height = 'auto';
+                    text.style.height = text.scrollHeight+'px';
 		}
-		
-		if (_counter < _number )
-			focusNextSlide();
-		
+		function delayedResize () {
+                    window.setTimeout(resize, 0);
+		}
+            observe(text, 'change',  resize);
+            observe(text, 'cut',     delayedResize);
+            observe(text, 'paste',   delayedResize);
+            observe(text, 'drop',    delayedResize);
+            observe(text, 'keydown', delayedResize);
+            resize();
 	}
-	
-	function initializeButtons() {
-		if ($("#form-section-next").is(":visible"))
-			$("#form-section-submit").hide();
-	}	
-	
-	$('.form-input').keypress(function (e) {
-		if (e.which == 13) {
+        
+        $('.form-input').keypress(function (e) {
+		if (e.which === 13) {
 			e.preventDefault();
 			return enterPressed();
 		}
 	});
 	
-	function checkSection() {
-		
-	}
-	
-	function checkInput(section ,input) {
-		console.log("Checking input: " + section + " - " + input);
-		console.log("Value: " + $("#input-" + section + "-" + input).val());
-		
-		if ($("#input-" + section + "-" + input).val() == "") {
-			return true;
-		}
-		
-		return false;
-	}
-	
-	function focusNextInput() {
-		for (var i = 0; i < $("#form-step-" + _counter).attr("data-input-number"); i++) {
-			if ($("#input-" + _counter + "-" + i).val() == "") {
-				$("#input-" + _counter + "-" + i).focus();
-				return false;
-			}
-		}
-	}
-	
-	function focusNextSlide() {
-		_counter++;
-		
-		$("#form-wrapper").scrollTo($("#form-step-" + _counter), 1000);
-		
-		if (_counter == _number) {
-			$("#form-section-next").hide("slow");
-			$("#form-section-submit").show("slow");
+	$("#form-section-submit").click(function() {
+		console.log("Sending form");
+		$("#form-input-submit").click();
+	});	
 
-		} else {
-			$("#input-" + _counter + "-0").focus();
-		}
-	}
-	
-	jQuery.fn.scrollTo = function(elem, speed) { 
-		$(this).animate({
-			scrollTop:  $(this).scrollTop() - $(this).offset().top + $(elem).offset().top 
-		}, speed == undefined ? 1000 : speed); 
-		return this; 
-	};
-	
-	$(".form-input-radio").on("change", function() {
-	   console.log("Checked input: " + $(this).attr("value"));
-	   console.log("Next section: " + $(this).attr("data-input-next"));
-	   
-	   var next = $(this).attr("data-input-next");
-	   _counter = next;
-	   
-	   $("#form-step-" + _counter).toggleClass("inactive", false);
-	   $("#form-wrapper").scrollTo($("#form-step-" + _counter), 1000);
-	});
-	
 	$("#contact-form").submit(function(e) {	
 		var dataString = $("#contact-form").serialize();
 		hideAllSections("#contact-form");
@@ -130,7 +62,7 @@ $(document).ready(function() {
 				
 			},
 			error: function(msg) {
-				console.log('not working '+msg);
+				console.log('not working '+msg.toString());
 			}
 		});
 		
@@ -141,20 +73,5 @@ $(document).ready(function() {
 		$(elem).children().hide(); 
 	}
 	
-		function initTextarea() {
-		var text = document.getElementById('input-5-0');
-		function resize () {
-			text.style.height = 'auto';
-			text.style.height = text.scrollHeight+'px';
-		}
-		function delayedResize () {
-			window.setTimeout(resize, 0);
-		}
-		observe(text, 'change',  resize);
-		observe(text, 'cut',     delayedResize);
-		observe(text, 'paste',   delayedResize);
-		observe(text, 'drop',    delayedResize);
-		observe(text, 'keydown', delayedResize);
-		resize();
-	}
+		
 });
